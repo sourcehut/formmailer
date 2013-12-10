465c12446590bfc4ad395bee69953e13e1101b51
anilkn <anilkn@yahoo.com>
2013-12-10 11:57:04 +0530

    signup

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ var SampleApp = function() {
 
         app.get('/', routes.index);
         app.get('/index.html', routes.index);
+        app.get('/signup', routes.index);
         
         // set up mailer routes
         require('./lib/routes/mailer.js')(app, db, '/mail');
